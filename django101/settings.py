e1c6f2ab2330c3cef0395349acf1541446b4f0ef
ESMAT001 <esmat10.niazi@gmail.com>
2021-02-24 09:17:07 +0430

    user form added

--- django101/settings.py
+++ django101/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'users.app.UsersConfig',
+    'users.apps.UsersConfig',
     'myapp.apps.MyappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
