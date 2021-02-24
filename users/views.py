2ba276be01e3dee2975d78ccd2eee428c33c305c
ESMAT001 <esmat10.niazi@gmail.com>
2021-02-24 09:29:31 +0430

    form data save feature added

--- users/views.py
+++ users/views.py
@@ -7,6 +7,7 @@ def register(request):
     if request.method=="POST":
         form=UserCreationForm(request.POST)
         if form.is_valid():
+            form.save()
             username=form.cleaned_data.get("username")
             messages.success(request,f'welcome user {username}')
             return redirect('../myapp/items/')
